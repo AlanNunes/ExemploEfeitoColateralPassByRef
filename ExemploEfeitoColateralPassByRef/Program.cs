ecf6703ddfa44214b5ff7e521ded8fde95cf468a
alannunes <alann.625@gmail.com>
2022-01-06 00:42:17 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploEfeitoColateralPassByRef/Program.cs
@@ -0,0 +1,19 @@
+﻿// Por ordem de chegada
+var filaDeEspera =
+    new List<string>() { "Gael", "Olívia", "Alan", "Miguel" };
+
+void MostraPorOrdemAlfabetica(List<string> fila)
+{
+    Console.WriteLine("Ordem alfabética");
+    fila.Sort();
+    fila.ForEach(nome => Console.WriteLine(nome));
+}
+
+void MostraPorOrdemDeChegada(List<string> fila)
+{
+    Console.WriteLine("Ordem de chegada");
+    fila.ForEach(nome => Console.WriteLine(nome));
+}
+
+MostraPorOrdemAlfabetica(filaDeEspera);
+MostraPorOrdemDeChegada(filaDeEspera);
